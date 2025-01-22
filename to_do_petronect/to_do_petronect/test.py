5a99b3dbf2e2e7f20a39ae03108bb76939efcd74
gacoelho <102633836+gacoelho@users.noreply.github.com>
2025-01-22 11:39:18 -0300

    Delete to_do_petronect/to_do_petronect/test.py

--- to_do_petronect/to_do_petronect/test.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
-
-postgres_url = os.environ.get('POSTGRES_URL')
-
-# Usando o valor obtido
-print(postgres_url)
